5e4789432be8c7b5f34109afca4c0d5bf54391bf
Smurf <sudaru2@gmail.com>
2019-10-20 21:40:45 +0300

    add IntPair struct and quick and merge sort for it

---
+++ IntPair.go
@@ -0,0 +1,81 @@
+package klpartitinlin
+
+type IntPair struct {
+	Number, Diff int
+}
+
+func QuickSortIntPair(arr *[]IntPair, _st, _en int) {
+	st := _st
+	en := _en - 1
+
+	p := (*arr)[_en>>1]
+	for ok := true; ok; ok = (st <= en) {
+		for (*arr)[st].Diff < p.Diff {
+			st++
+		}
+		for (*arr)[st].Diff > p.Diff {
+			en--
+		}
+
+		if st <= en {
+			temp := (*arr)[st]
+			(*arr)[st] = (*arr)[en]
+			(*arr)[en] = temp
+			st++
+			en--
+		}
+	}
+
+	if en > 0 {
+		QuickSortIntPair(arr, 0, en)
+	}
+	if _en > st {
+		QuickSortIntPair(arr, st, _en-st)
+	}
+
+}
+
+func MergeSortIntPair(arr *[]IntPair, _st, _en int) {
+	if _st < _en {
+		split := (_st + _en) / 2
+		MergeSortIntPair(arr, _st, split)
+		MergeSortIntPair(arr, split, _en)
+		Merge(arr, _st, split, _en)
+	}
+
+}
+
+func Merge(arr *[]IntPair, _st, _sp, _en int) {
+	posSt := _st
+	posEn := _sp + 1
+
+	posRes := 0
+
+	res := make([]IntPair, _en-_st+1)
+
+	for posSt <= _sp && posEn <= _en {
+		if (*arr)[posSt].Diff < (*arr)[posEn].Diff {
+			res[posRes] = (*arr)[posSt]
+			posSt++
+		} else {
+			res[posRes] = (*arr)[posEn]
+			posEn++
+		}
+		posRes++
+	}
+
+	for posEn <= _en {
+		res[posRes] = (*arr)[posEn]
+		posRes++
+		posEn++
+	}
+	for posSt <= _sp {
+		res[posRes] = (*arr)[posSt]
+		posSt++
+		posRes++
+	}
+
+	for posRes = 0; posRes < _en-_st+1; posRes++ {
+		(*arr)[_st+posRes] = res[posRes]
+	}
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/Rakiiii/goKLPartition
+
+go 1.13
+
+require (
+	github.com/Rakiiii/goBoolMatrix v0.1.2
+	github.com/Rakiiii/goGraph v0.1.0
+	github.com/Rakiiii/goGraphPartitionLib v0.1.4
+)
